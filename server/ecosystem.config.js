41167560edba5e8d45d7b98a9cffd268bd93188b
Aryanseth009 <setharyan53@gmail.com>
2025-03-08 17:02:17 +0530

    ecosystem file added

---
+++ server/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: 'server',
+        script: 'server.js',
+        interpreter: '/home/ubuntu/.nvm/versions/node/v18.20.7/bin/node', // Path to Node version from nvm
+      },
+    ],
+  };
+  
