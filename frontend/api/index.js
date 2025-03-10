7c0b5310e1eacb8c5a26f5f98f5944d64d4695b4
Aryanseth009 <setharyan53@gmail.com>
2025-03-10 16:26:24 +0530

    bkd and frt

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,5 +1,5 @@
 // This file is just a placeholder for Vercel
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(200).json({
     message: 'API is working!'
   });
